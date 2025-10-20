2c0663b5502662085784e23c62359f3f6cd80d69
mingyseven-oss <ming.y.seven@gmail.com>
2025-10-20 15:23:15 +0800

    update website

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -7,7 +7,7 @@ import { Document, Files, MagicStick, Picture, DataAnalysis, Film } from '@eleme
 const logo = './logo.png'
 
 // 标题
-const title = 'VisaDexGrasp'
+const title = 'VisaDexGrasp_VisaDexGrasp_'
 
 // 标题颜色
 const title_color = '#000000'
